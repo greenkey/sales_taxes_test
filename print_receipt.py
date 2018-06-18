cf77381b1d81daf9dec2114a9461062987b6df40
Lorenzo Mele <lorenzo@loman.it>
2018-06-18 14:32:16 +0200

    implemented correct round and testing final output

--- print_receipt.py
+++ print_receipt.py
@@ -1,32 +1,44 @@
 import fileinput
 import re
 from decimal import Decimal
+import math
+
 from sales_taxes import get_rate
 
 
 def main():
-    items = list()
+    items = [i for i in get_items_from_file(fileinput.input())]
 
-    for line in fileinput.input():
+    print(produce_final_output(items))
+
+
+def get_items_from_file(file_handler):
+    for line in file_handler:
         item = parse_input_line(line)
 
         item = add_taxes(item)
 
-        print("{} {}: {:.2f}".format(
+        yield item
+
+
+def produce_final_output(items):
+    output = []
+    for item in items:
+        output.append("{} {}: {:.2f}".format(
             item["quantity"],
             item["description"],
             item["price"] + item["taxes"],
         ))
 
-        items.append(item)
-
-    print("Sales Taxes: {:.2f}".format(
+    output.append("Sales Taxes: {:.2f}".format(
         sum([i["taxes"] for i in items])
     ))
 
-    print("Total: {:.2f}".format(
+    output.append("Total: {:.2f}".format(
         sum([i["total_price"] for i in items])
     ))
+
+    return "\n".join(output)
 
 
 def parse_input_line(line):
@@ -43,8 +55,9 @@ def parse_input_line(line):
 
 
 def add_taxes(item):
-    item["taxes"] = item["price"] * \
-        get_rate(item["description"]) * item["quantity"]
+    rate = get_rate(item["description"])
+    raw_tax = item["price"] * rate * item["quantity"]
+    item["taxes"] = math.ceil(raw_tax / Decimal("0.05")) * Decimal("0.05")
     item["total_price"] = item["taxes"] + item["price"] * item["quantity"]
     return item
 
