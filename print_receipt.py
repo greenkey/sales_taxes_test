0a63ec07a8577655db003d0e7ee0fcd60090158a
Lorenzo Mele <lorenzo@loman.it>
2018-06-18 12:56:22 +0200

    adding taxes to every line

--- print_receipt.py
+++ print_receipt.py
@@ -1,11 +1,20 @@
 import fileinput
 import re
 from decimal import Decimal
+from sales_taxes import get_rate
 
 
 def main():
     for line in fileinput.input():
-        print(line)
+        item = parse_input_line(line)
+
+        item = add_taxes(item)
+
+        print("{} {}: {}".format(
+            item["quantity"],
+            item["description"],
+            item["price"] + item["taxes"],
+        ))
 
 
 def parse_input_line(line):
@@ -19,6 +28,13 @@ def parse_input_line(line):
         "description": m.group(2).strip(),
         "price": Decimal(m.group(3)),
     }
+
+
+def add_taxes(item):
+    item["taxes"] = item["price"] * \
+        get_rate(item["description"]) * item["quantity"]
+    item["total_price"] = item["taxes"] + item["price"] * item["quantity"]
+    return item
 
 
 if __name__ == "__main__":
--- test_print_receipt.py
+++ test_print_receipt.py
@@ -16,3 +16,29 @@ def test_parse_input_line():
 
     p = parse_input_line("music CD at 14.99")
     assert p == None
+
+
+def test_add_taxes_to_item():
+    item = add_taxes({
+        "quantity": 1,
+        "description": "chocolate",
+        "price": Decimal("10"),
+    })
+    assert item["taxes"] == 0
+    assert item["total_price"] == 10
+
+    item = add_taxes({
+        "quantity": 5,
+        "description": "imported chocolate",
+        "price": Decimal("10"),
+    })
+    assert item["taxes"] == Decimal(10 * 0.05 * 5)
+    assert item["total_price"] == 50 + item["taxes"]
+
+    item = add_taxes({
+        "quantity": 10,
+        "description": "imported bottle of perfume",
+        "price": Decimal("100"),
+    })
+    assert item["taxes"] == Decimal(100 * 0.15 * 10)
+    assert item["total_price"] == 1000 + item["taxes"]
