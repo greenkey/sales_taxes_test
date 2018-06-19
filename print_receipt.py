74916d94bb3f97e26cb81c4798846a1736d7ad44
Lorenzo Mele <lorenzo@loman.it>
2018-06-19 12:20:39 +0200

    migrated all to using Item object

--- print_receipt.py
+++ print_receipt.py
@@ -3,7 +3,7 @@ import re
 from decimal import Decimal
 import math
 
-from sales_taxes import get_rate, is_imported
+from sales_taxes import get_rate, Item
 
 
 def main():
@@ -14,10 +14,8 @@ def main():
 
 def get_items_from_file(file_handler):
     for line in file_handler:
-        item = parse_input_line(line)
-
-        item = add_taxes(item)
-
+        item = Item()
+        item.parse_description(line)
         yield item
 
 
@@ -25,56 +23,17 @@ def produce_final_output(items):
     output = []
     for item in items:
 
-        description = item["clean_description"]
-        if item["imported"]:
-            description = "imported " + description
+        output.append(str(item))
 
-        output.append("{} {}: {:.2f}".format(
-            item["quantity"],
-            description,
-            item["price"] + item["taxes"],
-        ))
-
-    output.append("Sales Taxes: {:.2f}".format(
-        sum([i["taxes"] for i in items])
+    output.append("Sales Taxes: {}".format(
+        sum([i.total_tax for i in items])
     ))
 
-    output.append("Total: {:.2f}".format(
-        sum([i["total_price"] for i in items])
+    output.append("Total: {}".format(
+        sum([i.total_price for i in items])
     ))
 
     return "\n".join(output)
-
-
-def parse_input_line(line):
-    m = re.match(r"^([0-9]+) +(.*) +at +([0-9\.]+)", line)
-
-    if m is None:
-        return None
-
-    full_description = clean_description = m.group(2).strip()
-
-    imported = is_imported(full_description)
-    if imported:
-        clean_description = full_description.replace(
-            "imported", "").replace(
-            "  ", " ").strip()
-
-    return {
-        "quantity": int(m.group(1)),
-        "description": full_description,
-        "price": Decimal(m.group(3)),
-        "imported": imported,
-        "clean_description": clean_description
-    }
-
-
-def add_taxes(item):
-    rate = get_rate(item["description"])
-    raw_tax = item["price"] * rate * item["quantity"]
-    item["taxes"] = math.ceil(raw_tax / Decimal("0.05")) * Decimal("0.05")
-    item["total_price"] = item["taxes"] + item["price"] * item["quantity"]
-    return item
 
 
 if __name__ == "__main__":
--- test_print_receipt.py
+++ test_print_receipt.py
@@ -3,57 +3,6 @@ from decimal import Decimal
 from print_receipt import *
 
 
-def test_parse_input_line():
-    p = parse_input_line("1 book at 12.49")
-    assert p["quantity"] == 1
-    assert p["description"] == "book"
-    assert p["price"] == Decimal("12.49")
-
-    p = parse_input_line("100 box of imported chocolates at 11.25")
-    assert p["quantity"] == 100
-    assert p["description"] == "box of imported chocolates"
-    assert p["imported"] is True
-    assert p["clean_description"] == "box of chocolates"
-    assert p["price"] == Decimal("11.25")
-
-    p = parse_input_line("music CD at 14.99")
-    assert p is None
-
-
-def test_add_taxes_to_item():
-    item = add_taxes({
-        "quantity": 1,
-        "description": "chocolate",
-        "price": Decimal("10"),
-    })
-    assert item["taxes"] == 0
-    assert item["total_price"] == 10
-
-    item = add_taxes({
-        "quantity": 5,
-        "description": "imported chocolate",
-        "price": Decimal("10"),
-    })
-    assert item["taxes"] == Decimal(10 * 0.05 * 5)
-    assert item["total_price"] == 50 + item["taxes"]
-
-    item = add_taxes({
-        "quantity": 10,
-        "description": "imported bottle of perfume",
-        "price": Decimal("100"),
-    })
-    assert item["taxes"] == Decimal(100 * 0.15 * 10)
-    assert item["total_price"] == 1000 + item["taxes"]
-
-    item = add_taxes({
-        "quantity": 1,
-        "description": "1 imported bottle of perfume",
-        "price": Decimal("47.50"),
-    })
-    assert item["taxes"] == Decimal("7.15")
-    assert item["total_price"] == Decimal("54.65")
-
-
 def test_final_output():
     input_lines = [
         "1 book at 12.49",
